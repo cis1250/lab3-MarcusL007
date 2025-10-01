bba524390b832352f1a5866cd0ee9bec950aa4e6
Marcus Le <mle10@uoguelph.ca>
2025-10-01 11:45:06 -0400

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -5,3 +5,18 @@
 # Prompt the user for the number of terms.
 # Validate that the input is a positive integer.
 # Use a for loop to print the Fibonacci sequence up to that many terms.
+
+while True:
+  amount = input()
+  if amount.isDigit() == False or amount <= 0 or isinstance(amount, int) == False:
+    continue
+  else:
+    first = 0
+    second = 1
+    print(first)
+    print(second)
+    for i in range(int(amount) - 2):
+      print(first + second)
+      temp = first + second
+      first = second
+      second = temp
